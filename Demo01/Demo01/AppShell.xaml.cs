0e95e796a8f2f21ebeb19593b85ff2439d5671bf
Marko Koiduste <marko.koiduste@outlook.com>
2021-04-08 08:51:23 +0800

    Add project files.

---
+++ Demo01/Demo01/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Demo01.ViewModels;
+using Demo01.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Demo01
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Demo01/Demo01/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Demo01.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Demo01.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
